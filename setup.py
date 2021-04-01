37694d2ed6a60c22793ca5e0af4dc2af3edf5a9b
MathleteDev <mathletedev@gmail.com>
2021-04-01 14:25:23 -0700

    publishing

--- setup.py
+++ setup.py
@@ -7,11 +7,12 @@ setup(
     name="pymul",
     version="0.0.1",
     author="mathletedev",
+    author_email="mathletedev@gmail.com",
     url="https://github.com/mathletedev/pymul",
     license="Apache License 2.0",
     description="A Python machine learning library!",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=["pymul", "pymul/functions", "pymul/layers"],
-    install_requires=["numpy ~= 1.20.2"],
+    install_requires=["numpy >= 1.20.2"],
 )
